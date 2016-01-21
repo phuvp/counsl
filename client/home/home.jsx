8938f76a7ccb6d3dfedad110248264e74cf1be16
tyrion <phuvp@EASTCAMPUS-ONE-O-ONE-HUNDRED-NINETY.MIT.EDU>
2016-01-21 04:02:29 -0500

    fixed homepage, need to fix deploy crash

--- client/home/home.jsx
+++ client/home/home.jsx
@@ -6,38 +6,37 @@ var Dropzone = require('react-dropzone');
 LawFirms = new Mongo.Collection('lawfirms');
 
 var DropzoneDemo = React.createClass({
-    getInitialState: function () {
-        return {
-          files: []
-        };
-    },
+  getInitialState: function () {
+    return {
+      files: []
+    };
+  },
+  onDrop: function (files) {
+    this.setState({
+      files: files
+    });
+  },
 
-    onDrop: function (files) {
-      this.setState({
-        files: files
-      });
-    },
+  onOpenClick: function () {
+    this.refs.dropzone.open();
+  },
 
-    onOpenClick: function () {
-      this.refs.dropzone.open();
-    },
-
-    render: function () {
-        return (
-            <div>
-                <Dropzone ref="dropzone" onDrop={this.onDrop}>
-                    <div>Try dropping some files here, or click to select files to upload.</div>
-                </Dropzone>
-                <button type="button" onClick={this.onOpenClick}>
-                    Open Dropzone
-                </button>
-                {this.state.files.length > 0 ? <div>
-                <h2>Uploading {this.state.files.length} files...</h2>
-                <div>{this.state.files.map((file) => <img src={file.preview} /> )}</div>
-                </div> : null}
-            </div>
-        );
-    }
+  render: function () {
+    return (
+      <div id="dropzone">
+        <Dropzone ref="dropzone" onDrop={this.onDrop}>
+          <label>Try dropping some files here, or click to select files to upload.</label>
+        </Dropzone>
+        <button type="button" onClick={this.onOpenClick}>
+          Open Dropzone
+        </button>
+        {this.state.files.length > 0 ? <label>
+          <h2>Up {this.state.files.length} files...</h2>
+          <label>{this.state.files.map((file) => <img src={file.preview} /> )}</label>
+        </label> : null}
+      </div>
+    );
+  }
 });
 
 
@@ -62,29 +61,30 @@ var Home = React.createClass({
   render: function() {
     return (
       <div>
-        <p>Hi Lawfirm</p>
+        <form id="msform" class='center' className="newforms" onSubmit={this.handleSubmit} >
+          <fieldset>
+            <h1 className="fs-subtitle">Law Firm Site Generator</h1>
 
-        <form className="newforms" onSubmit={this.handleSubmit} >
-          Law Firm:
-          <input
-            type="text"
-            name="name"
-            placeholder="ELT Partners"
-            onChange={this.handleChange('name')}
-            value={this.state.name}/>
-          Email:
-        <input
-            type="text"
-            name="email"
-            placeholder="email"
-            onChange={this.handleChange('email')}
-            value={this.state.email}/>
+            <input
+              type="text"
+              name="name"
+              placeholder="ELT Partners"
+              onChange={this.handleChange('name')}
+              value={this.state.name}/>
 
+            <input
+              type="text"
+              name="email"
+              placeholder="email"
+              onChange={this.handleChange('email')}
+              value={this.state.email}/>
+            <DropzoneDemo />
 
-          <input
-            type="submit"
-            value="Submit"/>
-          <DropzoneDemo />
+            <input
+              type="submit"
+              className="submit action-button"
+              value="Submit"/>
+          </fieldset>
         </form>
 
       </div>
